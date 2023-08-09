93b96ad157b945b34b4c262ec7af6b71c161094d
Amey <ameykurade60@gmail.com>
2023-08-09 19:25:18 +0530

    Init

---
+++ demo/src/functdemo/FunctDemo.java
@@ -0,0 +1,13 @@
+package functdemo;
+import java.util.*;
+import java.lang.*;
+
+public class FunctDemo{
+    public static void main(String[] args) {
+        message();
+        System.out.println("Cry, and you stop the monotony");
+    }
+    static void message(){
+        System.out.println("Smile, and the World smiles with you...");
+    }
+}
