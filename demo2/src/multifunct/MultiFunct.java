1878c3350fd589977d67e89c31bfa7f208255a8e
Amey <ameykurade60@gmail.com>
2023-08-09 19:33:35 +0530

    demo 2 init

---
+++ demo2/src/multifunct/MultiFunct.java
@@ -0,0 +1,22 @@
+package multifunct;
+import java.lang.*;
+
+public class MultiFunct{
+    public static void main(String[] args) {
+        System.out.println("I am in main");
+        itly();
+        brazil();
+        India();
+    }
+    static void itly(){
+        System.out.println("I am in Itly");
+    }
+
+    static void brazil(){
+        System.out.println("I am in Brazil");
+    }
+
+    static void India(){
+        System.out.println("I am in India");
+    }
+}
